e0da1b6b8577bcd925556b926045b08d449a5435
web3online <placex.com@gmail.com>
2022-07-24 16:13:08 +0100

    use mutex to update the counter inside app shared state

--- placex-web-service/src/main.rs
+++ placex-web-service/src/main.rs
@@ -1,15 +1,22 @@
+use std::sync::Mutex;
 
 #[allow(unused_imports)]
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
-use placex_web_service::route::*;
+use placex_web_service::route::{*,State::{AppState}};
+
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()>{
     HttpServer::new(|| {
         App::new()
+        .app_data(web::Data::new(AppState{
+            app_name : String::from("PlaceX Service"),
+            counter : Mutex::new(0),
+        }))
         .service(hello)
         .service(echo)
         .route("/hey", web::get().to(manual_hello))
+        .service(increment_state)
         .service(web::scope("/app").route("/x", web::get().to(manual_hello) ))
         
     })
--- placex-web-service/src/route.rs
+++ placex-web-service/src/route.rs
@@ -1,7 +1,10 @@
+pub mod State;
 #[allow(unused_imports)]
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 #[allow(unused_imports)]
 use placex_model::*;
+use crate::route::State::AppState;
+
 #[get("/")]
 pub async fn hello() -> impl Responder{
     HttpResponse::Ok().body("Hello world")
@@ -14,4 +17,18 @@ pub async fn echo(req_body: String) -> impl Responder {
 
 pub async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
+}
+
+#[get("/")]
+pub async fn index(data: web::Data<AppState>) -> String{
+    let app_name = &data.app_name;
+    format!("Hello {app_name}!")
+}
+
+#[get("/increment_state")]
+pub async fn increment_state(data : web::Data<AppState>) -> impl Responder {
+    let mut counter = data.counter.lock().unwrap();
+    *counter +=1;
+    
+    HttpResponse::Ok().body(format!("{counter}"))
 }
