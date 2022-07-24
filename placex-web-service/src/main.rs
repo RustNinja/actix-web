4813690be481b5a5223778826363716c996e40af
web3online <placex.com@gmail.com>
2022-07-24 12:27:46 +0100

    extract route logic to a different module

--- placex-web-service/src/main.rs
+++ placex-web-service/src/main.rs
@@ -1,23 +1,17 @@
+
 #[allow(unused_imports)]
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use placex_web_service::route::*;
 
-#[get("/")]
-async fn hello() -> impl Responder{
-    HttpResponse::Ok().body("Hello world")
-}
-
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
-}
 #[actix_web::main]
 async fn main() -> std::io::Result<()>{
     HttpServer::new(|| {
-        App::new().service(hello).service(echo).route("/hey", web::get().to(manual_hello))
+        App::new()
+        .service(hello)
+        .service(echo)
+        .route("/hey", web::get().to(manual_hello))
+        .service(web::scope("/app").route("/x", web::get().to(manual_hello) ))
+        
     })
     .bind(("127.0.0.1",8080))?
     .run()
